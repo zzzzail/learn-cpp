8d07ef3c207551b11319e98f7e8932a9e3edebe9
Zail <zzzzail.mail@gmail.com>
2020-12-11 16:49:42 +0800

    1

--- c/lesson/01hello.c
+++ c/lesson/01hello.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-    
+/**
+ * \n 表示换行
+ * 
+ */
+int main() {
+
     printf("Hello World!");
 
     return 0;
 }
 
-/*
-将hello.c进行预处理、编译、汇编和链接，最终生成可执行的目标文件。
-编译命令：
-gcc -o 01hello 01hello.c
-
-*/
---
+++ c/lesson/05number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * %d d表示digital
+ */
+int main(int argc, char const *argv[])
+{
+    printf("21 + 56 = %d\n", 21 + 56);
+
+    return 0;
+}
