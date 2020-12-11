e6e464b72c78e5ab084eea5f676fda24c775704f
Zail <zzzzail.mail@gmail.com>
2020-12-11 23:12:52 +0800

    1

---
+++ c/lesson/16revertnumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int x;
+    scanf("%d", &x);
+
+    int digit;
+    int ret = 0;
+    while (x > 0) {
+        digit = x % 10;
+        printf("%d", digit);
+        ret = ret * 10 + digit;
+        x /= 10;
+    }
+    printf("\n");
+
+    return 0;
+}
