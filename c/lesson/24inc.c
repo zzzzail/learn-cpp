7c2bd72fb7a4449193d72808e370903fcc676b54
Zail <zzzzail.mail@gmail.com>
2020-12-14 08:48:25 +0800

    1

---
+++ c/lesson/24inc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "max.h"
+
+int main() {
+    int a = 10;
+    int b = 20;
+    printf("%d", max(a, b));
+
+    return 0;
+}
+
---
+++ c/lesson/max.h
@@ -0,0 +1,7 @@
+#ifndef _MAX_H_
+#define _MAX_H_
+
+// 获得两个值大的那一个
+int max(int a, int b);
+
+#endif
