5c0640bb52f8231d4e1cefcfbd904dea6becd65f
Zail <zzzzail.mail@gmail.com>
2021-01-26 10:49:10 +0800

    1

--- c/lesson/37.c
+++ c/lesson/37.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
-    char ch1, ch2;
-    scanf("%3c%3c", &ch1, &ch2);
-    printf("ch1=%c;ch2=%c\n", ch1, ch2);
+    // char ch1, ch2;
+    // scanf("%3c%3c", &ch1, &ch2);
+    // printf("ch1=%c;ch2=%c\n", ch1, ch2);
+
+    // 字符指针
+    char *p;
+    p = "hahahhah";
+    puts(p);
+
+    // 这是字符数组
+    char arr[80] = {"hahahah"};
+    puts(arr);
 
     return 0;
 }
