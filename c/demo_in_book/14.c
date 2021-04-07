23c6527127ea2ac27962f9a2e8c9605cd5fb80a8
zail <zzzzail.mail@gmail.com>
2021-04-07 08:59:13 +0800

    1

--- c/demo_in_book/14.c
+++ c/demo_in_book/14.c
@@ -7,7 +7,10 @@ int main() {
 
     int p = 2, q = 4, *r;
     r = fun(&p, &q);
-    printf("%d", *r);
+    printf("%d\n", *r);
+
+    int a[3][3] = {1, 2, 3, 4, 999, 6, 7, 8, 9};
+    printf("%d\n", *(*(a + 1) + 1));
 
     return 0;
 }
